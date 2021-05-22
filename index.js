b8df6e427bc3cd85cb4f352307b86bf0513f9515
MSGuy01 <msguy01@outlook.com>
2021-05-22 11:06:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const Discord = require('discord.js');
+const bot = new Discord.Client();
 const auth = require('./auth.json');
-const bot = new Discord.Client;
+const botID = "662739267943923734";
 
 bot.login(auth.token);
 
-bot.on('login', () => {
+bot.on('ready', () => {
     bot.user.setActivity('Cyberbullying Discord users');
     console.log('online');
+});
+
+bot.on('message', message => {
+    var args;
+    const msg = message.content;
+    if (message.author.id != botID) {
+        for (var i = 0; i < msg.length; i++) {
+            if (msg.substring(i, i + 4) == 'like' || msg.substring(i, i + 4) == 'love') {
+                args = msg.substring(i + 4, msg.length);
+                message.channel.send('I hate' + args);
+                break;
+            }
+            else if (msg.substring(i, i + 4) == 'hate') {
+                args = msg.substring(i + 4, msg.length);
+                message.channel.send('I like' + args);
+                break;
+            }
+            else if (msg.substring(i, i + 9) == 'dont like') {
+                args = msg.substring(i + 9, msg.length);
+                message.channel.send('I like' + args);
+                break;
+            }
+        }
+    }
 });
